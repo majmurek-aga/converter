49a6549da34c08961ac2d6cb81bb1211e0ea03b4
Agnieszka Majmurek <majmurek.agnieszka@gmail.com>
2023-03-27 19:39:36 +0200

    init

---
+++ js.js
@@ -0,0 +1,10 @@
+const conventer = document.querySelector('#converter');
+const result = document.querySelector('.result');
+const convBtn = document.querySelector('.conv');
+const resetBtn = document.querySelector('.reset');
+const changeBtn = document.querySelector('.change');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+
+// T(°F) = T(°C) x 1.8 + 32
+// T(°C) = (T(°F) - 32) / 1.8
